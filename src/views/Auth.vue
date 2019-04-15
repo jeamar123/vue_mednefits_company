d34687643a72782f90c0ac5dcab6489c206af709
noelou jan <noelounagac@gmail.com>
2019-04-15 18:38:05 +0800

    add ui for auth

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,6 +1,36 @@
 <template>
 	<div class="auth-container">
-	  sample
+	  
+	  <div class="log-in-wrapper">
+	  	<div class="log-in-need-help-container">
+	  		<a href="#" class="need-help-text"><h3>Need help?</h3></a>
+	  	</div>
+	  	<div class="portal-log-in-container">
+	  		<div class="portal-img-text-container">
+	  			<img :src="'../assets/img/mednefits_logo_v3_(white).png'">
+	  			<h2 class="text-below-image">for business</h2>
+	  		</div>
+	  		<form>
+  				<div class="form-group">
+  					<input class="login-input" type="text" placeholder="Email address">
+  				</div>
+  				<div class="form-group">
+  					<input class="login-input" type="text" placeholder="Enter password">
+  				</div>
+  				<div class="login-checkbox">
+  					<label>
+  						<input type="checkbox"> Stay signed in
+  					</label>
+  				</div>
+  				<div class="login-btn-container">
+  					<button>Log in</button>
+  				</div>
+  				<div class="forgot-password-container">
+  					<a href="">Forgot password?</a>
+  				</div>
+	  		</form>
+	  	</div>
+	  </div>
 
   </div>
   
